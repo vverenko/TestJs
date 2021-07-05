38c6bccd781c8d0b9b862edf25c6d11f34fc18c3
Volodymyr <Verenko2000@gmail.com>
2021-07-05 23:27:29 +0300

    made tasks

--- 2.js
+++ 2.js
@@ -1,36 +1,22 @@
-const array1 = [2, 3, 5, 9, 1, 0];
-const array2 = [66, 54, 45, 89, 32, 76, 11];
-const array3 = [2];
-const array4 = [5, 8, -11, 7, -9];
-const array5 = [];
-const array6 = 'test';
-const array7 = '';
-
 /**
- * 
- * @param {numer[]} arr 
- * @returns {null|number}
+ * adding all occurrences in the sequence
+ * @param {number} val 
+ * @returns {number|string}
  */
-const getAverage = arr => {
-    if (!(arr instanceof Array)) {
-        console.error('it is not array');
-        return null;
-    }
-    if (!arr.length) {
-        console.error('array is empty');
-        return null;
-    }
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
-    }
-    return sum / arr.length;
+const sumNumber = val => {
+
+    if (typeof val !== 'number' || !!(val % 1) || val < 0) return `error value ${val}`;
+
+    if (val === 0) return 0;
+
+    return val + sumNumber(val - 1);
 }
 
-console.log(getAverage(array1)); // 3.3333333333333335
-console.log(getAverage(array2)); // 53.285714285714285
-console.log(getAverage(array3)); // 2
-console.log(getAverage(array4)); // 0
-console.log(getAverage(array5)); // array is empty \n null
-console.log(getAverage(array6)); // it is not array \n null
-console.log(getAverage(array7)); // it is not array \n null
+
+console.log(sumNumber(1)); // 1
+console.log(sumNumber(5)); // 15
+console.log(sumNumber(10)); // 55
+console.log(sumNumber(100)); // 5050
+console.log(sumNumber('rgdfg')); // error value rgdfg
+console.log(sumNumber(null)); // error value null
+console.log(sumNumber(undefined)); // error value undefined
